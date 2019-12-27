974590f496881198fbb592954d0310df6f42e60b
Omer Katan <omerkatan@live.com>
2019-12-26 19:17:30 -0800

    push

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ var app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+
+app.listen(PORT);
