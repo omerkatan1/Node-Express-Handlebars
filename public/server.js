2d66a891b0cf03aa4305951ba0fd2a37d35f8ec7
Omer Katan <omerkatan@live.com>
2019-12-26 18:39:16 -0800

    push

--- public/server.js
+++ public/server.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
+
+var app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
