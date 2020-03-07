e57cbee0334002222674d28d16c33f783eb9d6c5
Omer Katan <omerkatan@Omers-MBP.home>
2020-03-06 22:13:47 -0800

    push

--- public/js/burgerFunctions.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $('#burgerSubmit').click(function() {
-        
-    });
-});
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require("express-handlebars");
-// const mysql = require("mysql");
+const mysql = require("mysql");
 
 
 
@@ -17,22 +17,26 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-// creates mysql connection
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: "3306",
-//     user: "root",
-//     password: "omer171015415",
-//     database: "burger_db"
-// });
+// creates sql connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "omer171015415",
+    database: "burger_db"
+});
 
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("connected as " + connection.threadId);
-// })
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as " + connection.threadId);
+})
 
 app.get("/", function(req, res) {
-    res.render("index");
+    connection.query("SELECT * FROM notDevoured;", function(err, data) {
+        if (err) throw err;
+
+        res.render("index", { notDevoured: data });
+    });
 });
 
 
